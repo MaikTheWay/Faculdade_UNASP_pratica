db3008459a4bf99a66655b0983a59b014222a7bc
MaikTheWay <108753254+MaikTheWay@users.noreply.github.com>
2023-04-25 15:09:24 -0300

    Update Exercicios Unasp Python.py

--- Exercicios Unasp Python.py
+++ Exercicios Unasp Python.py
@@ -267,26 +267,239 @@ elif plado != slado != tlado:
 
 elif plado == slado or slado == tlado or plado == tlado:
   print('triângulo isósceles')
-  
-  
+ 
+ 
 
-# Escreva um programa para jogar jokenpô com o computador.
-import random
-
-opcoes = ['papel', 'pedra', 'tesoura']
-
-escolha_computador = random.choice(opcoes)
-user = input("Escolha entre: papel, pedra, tesoura \n")
-
-if user == "papel" or user == 'pedra' or user == 'tesoura':
-  if user == "papel" and escolha_computador == "tesoura":
-    print( "Voce perdeu")
-  elif user == 'pedra' and escolha_computador == "papel":
-    print("Voce perdeu")
-  elif user == "tesoura" and escolha_computador == "pedra":
-    print("Voce perdeu")
+# Crie uma função recursiva para calcular a soma de uma lista de números.
+def soma_lista_recursiva(lista):
+  if len(lista) == 1:
+    return lista[0]
   else:
-    print("ganhou")
-print("o cpu escolheu:", escolha_computador)
+    return lista[0] + soma_lista_recursiva(lista[1:])
+
+soma_lista_recursiva([1,3,4,5])
 
 
+
+# Crie uma função recursiva para obter o fatorial de um número inteiro não negativo.
+def fatorial(n):
+  if n < 0:
+    print('Não definido. ')
+  elif n <= 1:
+    return 1
+  else:
+    return n * fatorial(n-1)
+
+fatorial(5)
+
+
+
+# Crie uma função recursiva para resolver a sequência de Fibonacci.
+# A soma dos 2 numeros anteriores é o resultado do proximo
+# Criar uma função recursiva
+
+def fibonacci(n):
+  if n == 0 or n == 1:
+    return n
+  else:
+    return (fibonacci(n-2) + fibonacci(n-1))
+
+fibonacci(5)
+
+
+
+# Crie uma função recursiva para calcular a soma dos dígitos de um número inteiro positivo.
+# 315 --> 9
+# 3 + 1 + 5 = 9
+def numero(num):
+  if num == 0:
+    return num
+  else:
+    return (num % 10) + numero(num // 10)
+
+numero(222)
+
+
+
+#Crie uma função recursiva que busca um elemento dentro de uma lista ordenada e retorna o índice desse elemento. 
+# Caso o índice não seja encontrado a função deve retornar o valor -1. Essa função precisa estar baseada no algoritmo de busca binária.
+def encontra_n_lista(n, lista, i):
+  if lista[0] == n:
+    return i 
+  else:
+    return encontra_n_lista(n, lista[1:], i + 1)
+
+encontra_n_lista(6, [1,2,3,4,5,6,7], 0)
+
+
+
+# Crie uma função que busca um elemento dentro de uma lista ordenada e retorna o índice desse elemento. 
+# Caso o índice não seja encontrado a função deve retornar o valor -1. Essa função precisa estar baseada no algoritmo de busca binária.
+# X = [2, 3, 23, 30]
+# 
+# correta:
+# Busca (23) ---> é para retornar 2 (Indice)
+#
+# errada
+# Busca (13) ---> não vai retornar nada, pois nâo há o 13 na lista
+
+def binario(array, item, begin=0, end=None):
+    if end is None:
+        end = len(array)-1
+    if begin <= end:
+        m = (begin + end)//2
+        if array[m] == item:
+            return m
+        if item < array[m]:
+            return binario(array, item, begin, m-1)
+        else:
+            return binario(array, item, m+1, end)
+    return "-1"
+
+lista = [2,3,4,5,6,7,8,9,10]
+binario(lista, 3)
+
+
+
+# Crie uma função que faça da divisão de dois números. Utilize os algoritmos de aproximação e de busca binária para criar essa função.
+# def dividir(a,b)
+#      return a/b
+
+def divisao(dividendo, divisor):
+
+  epislon = 0.01
+  inf = 0 
+  sup = 100
+  chute = (sup + inf)/2 
+ 
+  while abs(chute * divisor - dividendo) >= epislon:
+    if chute * divisor < dividendo: 
+      inf = chute
+    else: 
+      sup = chute
+    chute = (sup + inf)/2
+  return chute 
+
+
+
+# Crie uma função que calcule a raíz quadrada de um número utilizando os algoritmos de aproximação e de busca binária.
+def raiz(raiz):
+
+  epislon = 0.01
+  inf = 0 
+  sup = raiz
+  chute = (sup + inf)/2 
+
+  while abs(chute**2 - raiz) >= epislon:
+    if chute**2 < raiz:
+      inf = chute
+    else: 
+      sup = chute
+    chute = (sup + inf)/2
+  return chute 
+
+
+
+# Dada uma lista de números inteiros únicos e não ordenados, crie uma função para encontrar todos os pares em que a diferença seja igual a k.
+# [1, 5, 4, -2, 6, 2]
+# k = 3
+# -------------------
+# (1, -2)
+# (5, 2)
+# (1, 4)
+def binario(array, diferenca, item, begin=0, end=None):
+    if end is None:
+        end = len(array)-1
+    if begin <= end:
+        m = (begin + end)//2
+        if array[m] - item == diferenca:
+            print(f'({array[m]}, {item})')
+        if item < array[m]:
+            return binario(array, diferenca, item, begin, m-1)
+        else:
+            return binario(array, diferenca, item, m+1, end)
+    return "-1"
+
+lista = [1, 5, 4, -2, 6, 2]
+lista.sort()
+for num in lista: 
+  binario(lista, 3, num)
+  
+  
+  
+# Você recebe uma lista com números inteiros positivos onde cada número diferente de zero representa uma coluna com largura igual a 1. 
+# Imagine que água é derramada sobre as colunas. Escreva um programa que calcule a área da superfície de água que está armazenada entre as colunas.
+# Dica: Você pode precisar usar as funções max() e min().
+
+# max(3, 10)
+# min(3, 10)
+
+vet_agua = [0,0,5,0,0,6,0]
+i = 0
+calc_nivel = []
+soma_nivel = 0
+ver_esquerda = 0
+
+for elem in vet_agua:
+  if i != len(vet_agua) - 1:
+    ver_direita = max(vet_agua[i+1:])
+  else:
+    ver_direita = 0
+
+  if i != 0:
+    ver_esquerda = max(vet_agua[:i])
+
+  if ver_direita >= ver_esquerda:
+     result = ver_esquerda - elem  
+  else:
+    result = ver_direita - elem                       
+  if result > 0:
+    soma_nivel += result
+
+  i += 1
+print(soma_nivel)
+
+
+
+# Crie um programa que define se um número interiro positivo é divisível por 3. Não é permitido usar os operadores de divisão (/, //, %).
+numero = int(input('Insira o numero: '))
+msg = 'n eh divisel por 3'
+while numero >= 3:
+  if numero == 3:
+    msg = 'eh divisivel por 3'
+  
+  numero -= 3
+
+print(msg)
+
+
+
+# Você recebe uma lista com números inteiros positivos que representam a quantidade de tempo que uma tarefa demora para ser executada.
+# Apenas uma tarefa pode ser executada por vez, mas as tarefas podem ser executadas em qualquer ordem. O tempo de espera de cada tarefa é
+# definido como a quantidade de tempo que a tarefa precisa esperar antes de sua execução ser iniciada.
+# Escreva um programa que retorna o tempo de espera mínimo para uma lista de atividades
+tempo_atv = [8,2,1,6]
+tempo_espera = []
+tempo_atv.sort()
+soma_espera = 0
+
+for pos in tempo_atv:
+  tempo_espera.append(soma_espera)
+  soma_espera += pos
+
+print('Tempo de espera mínimo: ', sum(tempo_espera))
+
+
+
+# Escreva um programa que recebe uma string de letras minúsculas e retorna o índice da primeira letra não repetida.
+# A primeira letra não repetida é a primeira letra da string que ocorre apenas uma vez.
+# Se a string não apresentar nenhuma letra não repetida, o programa deve retornar -1.
+lista = ['a', 'v', 'b', 'a', 'c', 'a', 'b']
+letra_distinta = '-1' 
+
+for letra in lista:
+  if lista.count(letra) == 1:
+    letra_distinta = letra
+    break
+
+print(letra_distinta)
